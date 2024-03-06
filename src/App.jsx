bbf90ea13b8f8e92d951bfc17d54b73d70fbd867
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-03-07 00:06:03 +0700

    fix: remove redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css';
-import {Provider} from 'react-redux'
-import {store} from './redux/store';
 import {NextUIProvider} from "@nextui-org/react";
 import Router from './routes/router';
 
 function App() {
   return (
-    <Provider store={store}>
-      <NextUIProvider>
-        <Router />
-      </NextUIProvider>
-    </Provider>
+    <NextUIProvider>
+      <Router />
+    </NextUIProvider>
   );
 }
 
