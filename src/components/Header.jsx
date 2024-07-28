27f033e328ade00351cc4a6b0c37f874ab53a617
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-07-28 20:05:34 +0700

    feat: make mobile and desktop header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,26 @@ import Nav from './Nav'
 const Header = () => {
   return (
     <header className='py-8 xl:py-12 text-white'>
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href='/'>
           <h1 className='text-4xl font-semibold'>
             Ichsan<span className='text-accent'>.</span>
           </h1>
         </Link>
 
-        <Nav />
+        {/* Desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href='/contact'>
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile nav */}
+        <div className='xl:hidden'>
+          mobile nav
+        </div>
+
       </div>
     </header>
   )
