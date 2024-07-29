06ebd7493b9f7f91a8e699d4c618a21bf2a4d029
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-07-29 23:42:16 +0700

    feat: add skills section on resume

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -2,12 +2,13 @@
 
 import {FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs} from 'react-icons/fa'
 import {SiTailwindcss, SiNextdotjs, SiExpress} from 'react-icons/si'
+import {motion} from 'framer-motion'
 
+// component
 import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/tabs'
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from '@/components/ui/tooltip'
 import {ScrollArea} from '@/components/ui/scroll-area'
 
-import {motion} from 'framer-motion'
 
 const about = {
   title: "About me",
@@ -140,6 +141,7 @@ const Resume = () => {
           </TabsList>
 
           <div className="min-h-[70vh] w-full">
+            {/* experience */}
             <TabsContent value="experience" className="w-full">
               <div className='flex flex-col gap-[30px] text-center xl:text-left'>
                 <h3 className='text-4xl font-bold'>{experience.title}</h3>
@@ -162,12 +164,60 @@ const Resume = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
+
+            {/* education */}
             <TabsContent value="education" className="w-full">
-              education
+              <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                <h3 className='text-4xl font-bold'>{education.title}</h3>
+                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{education.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
+                    {education.items.map((item, index) => {
+                      return (
+                        <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
+                          <span className='text-accent'>{item.duration}</span>
+                          <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>{item.degree}</h3>
+                          <div className='flex items-center gap-3'>
+                            <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
+                            <p className='text-white/60'>{item.institution}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
-            <TabsContent value="skills" className="w-full">
-              skills
+
+            {/* skills */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                  <h3 className='text-4xl font-bold'>{skills.title}</h3>
+                  <p className='max-w-[600p] text-white/60 mx-auto xl:mx-0'>{skills.description}</p>
+                </div>
+                <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]'>
+                  {skills.skillList.map((item, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className='w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group'>
+                              <div className='text-6xl group-hover:text-accent transition-all duration-300'>{item.icon}</div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className='capitalize'>{item.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
             </TabsContent>
+
+            {/* about */}
             <TabsContent value="about" className="w-full">
               about
             </TabsContent>
