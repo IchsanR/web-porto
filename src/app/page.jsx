180189e8ad734f3f927e32c1a3a5d35df6acfe06
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-07-29 19:29:25 +0700

    feat: add stats section on homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,16 +4,18 @@ import {FiDownload} from 'react-icons/fi'
 // Components
 import Social from "@/components/Social"
 import Photo from "@/components/Photo"
+import Stats from "@/components/Stats"
 
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
-            <h1 className="h1 mb-6">Hello I'm <br /><span className="text-accent">Ichsan Ramadhan</span>
+            <h1 className="h1 mb-6">Hello, I'm <br /><span className="text-accent">Ichsan Ramadhan</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">Currently working as Frontend Developer at Bank Rakyat Indonesia. Possess the
               necessary skills as a Software Developer</p>
@@ -33,13 +35,13 @@ const Home = () => {
             </div>
           </div>
 
-
           {/* photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   )
 }
